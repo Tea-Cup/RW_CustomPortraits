7a2f936f625f1f1fc1fca909a99941832e8f1287
Tea-Cup <cozyteacup@pm.me>
2025-09-07 03:50:10 +0900

    [WIP] Base condition drawer

--- 1.6/Source/ConstantPortraitCondition.cs
+++ 1.6/Source/ConstantPortraitCondition.cs
@@ -1,13 +1,17 @@
-﻿using Verse;
+﻿using UnityEngine;
+using Verse;
 
 namespace Foxy.CustomPortraits {
 	public class ConstantPortraitCondition : PortraitCondition {
-		public override string TranslationKey => "Foxy.CustomPortraits.Condition.Constant";
+		public const string TranslationKey = "Foxy.CustomPortraits.Condition.Constant";
 
 		public ConstantPortraitCondition() { }
 		public ConstantPortraitCondition(PortraitPosition? position, string filename) {
 			this.position = position;
 			this.filename = filename;
+		}
+		public override void Draw(Pawn p, Rect left, Rect center, Rect right) {
+			base.Draw(p, left, center, right);
 		}
 
 		public override bool CheckFor(Pawn p) {
--- 1.6/Source/PortraitCondition.cs
+++ 1.6/Source/PortraitCondition.cs
@@ -1,12 +1,34 @@
-﻿using Verse;
+﻿using UnityEngine;
+using Verse;
 
 namespace Foxy.CustomPortraits {
 	public abstract class PortraitCondition : IExposable {
-		public abstract string TranslationKey { get; }
 		public PortraitPosition? position = null;
 		public string filename = null;
 
+		private static readonly Color borderEmpty = new Color(0.5f, 0.5f, 0.5f);
+		private static readonly Color borderError = new Color(0.7f, 0.3f, 0.3f);
+		private static readonly Color hover = new Color(1, 1, 1, 0.3f);
+
 		public abstract bool CheckFor(Pawn p);
+		public virtual void Draw(Pawn p, Rect left, Rect center, Rect right) {
+			Texture2D tex = PortraitCache.Get(filename);
+			if (tex != null) {
+				GUI.DrawTexture(right, tex);
+			} else {
+				GUI.color = filename == null ? borderEmpty : borderError;
+				Widgets.DrawBox(right, filename == null ? 1 : 2);
+			}
+			if(Mouse.IsOver(right)) {
+				GUI.color = hover;
+				GUI.DrawTexture(right, BaseContent.WhiteTex);
+			}
+			GUI.color = Color.white;
+			if (Widgets.ButtonInvisible(right, true)) {
+				// TODO: file selector
+				Log.Message("Click");
+			}
+		}
 
 		public virtual string GetTexture(Pawn p) {
 			return filename;
